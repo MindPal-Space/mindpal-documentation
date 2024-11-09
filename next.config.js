5c9e967b2acf20df307603e42d50e9e23b7d4785
Phạm Quỳnh Trang <trangpq2310@gmail.com>
2024-11-09 22:10:00 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-module.exports = withNextra()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default async () => {
+  const fs = await import('fs/promises');
+  const { default: nextra } = await import('nextra');
+  const nextraConfig = JSON.parse(
+    await fs.readFile(path.join(__dirname, 'nextra.config.js'), 'utf8')
+  );
+
+  return {
+    ...nextra(),
+    ...nextraConfig
+  };
+};
