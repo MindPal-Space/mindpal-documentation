51721ebba28ba083237be9c5329b03980beb3622
sylviangth <tham080402@gmail.com>
2025-02-21 21:47:57 +0700

    update

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -2,10 +2,14 @@ import { Callout } from "nextra/components";
 
 export default function () {
   return (
-    <Callout
-      emoji={"👋"}
-    >
-      MindPal is a platform that helps you build AI agents & multi-agent workflows to automate any tasks with custom data. <strong><a href="https://mindpal.space/" target="_blank">Get started now for free here.</a></strong>
+    <Callout emoji={"👋"} type="info">
+      MindPal is a platform that helps you build AI agents & multi-agent
+      workflows to automate business processes.{" "}
+      <strong>
+        <a href="https://mindpal.space/" target="_blank">
+          Get started here!
+        </a>
+      </strong>
     </Callout>
-  )
+  );
 }
